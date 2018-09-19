de65a7b5cfee52fe886cd4d5af681b07c4b2d390
libo <libo@sudiyi.cn>
2018-09-19 18:17:02 +0800

    提交

---
+++ src/main/java/com/example/client/controller/HahaController.java
@@ -0,0 +1,26 @@
+package com.example.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: libo
+ * @Mail: libo@sudiyi.cn
+ * @Date: 2018/9/19 17:59
+ * @Version: 1.1.5
+ * @Description:
+ */
+@RestController
+@RefreshScope
+public class HahaController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/get")
+    public String get(){
+        return this.profile;
+    }
+}
